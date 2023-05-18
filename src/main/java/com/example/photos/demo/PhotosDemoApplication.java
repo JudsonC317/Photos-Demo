42b5b38ff778d5e2b6c9ecd99992c08ca6277731
Judson Cooper <judsoncooper317@gmail.com>
2023-05-18 15:53:17 -0400

    Spring Initializr

---
+++ src/main/java/com/example/photos/demo/PhotosDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.photos.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotosDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/photos/demo/PhotosDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotosDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
