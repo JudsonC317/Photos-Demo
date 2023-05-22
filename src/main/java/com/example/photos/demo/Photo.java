22369251aafac5a12ecb6c82583de9ea8894af9d
Judson Cooper <judsoncooper317@gmail.com>
2023-05-22 15:34:01 -0400

    Add Photo model

---
+++ src/main/java/com/example/photos/demo/Photo.java
@@ -0,0 +1,34 @@
+package com.example.photos.demo;
+
+public class Photo {
+
+    private String id;
+
+    private String fileName;
+
+    // TODO: raw data
+    
+    public Photo() {
+    }
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
