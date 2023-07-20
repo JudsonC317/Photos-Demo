4528fbd182c58387e89e0382e8dd3bc0f6ba57c9
Judson Cooper <judsoncooper317@gmail.com>
2023-07-20 14:01:30 -0400

    Add post mapping for Photo

--- src/main/java/com/example/photos/demo/PhotosController.java
+++ src/main/java/com/example/photos/demo/PhotosController.java
@@ -3,13 +3,16 @@ package com.example.photos.demo;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class PhotosController {
@@ -39,5 +42,13 @@ public class PhotosController {
     public void delete(@PathVariable String id) {
         Photo photo = db.remove(id);
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/photos")
+    public Photo create(@RequestBody Photo photo) {
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+
+        return photo;
     }
 }
